df625e63fddcbedd1147e4aece354dba2c6460ad
Sara Moreira <a8544@alunos.ipca.pt>
2022-03-30 23:17:03 +0100

    Add project files.

---
+++ FindMyMed/Models/Account.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FindMyMed.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public StatusEnum Status { get; set; }
+        public Types Type { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+    }
+
+    public enum StatusEnum
+    {
+        Activo,
+        Inactivo,
+    }
+    public enum Types
+    {
+        [Display(Name = "Utilizador")]
+        User,
+        [Display(Name = "Farmácia")]
+        Pharm,
+    }
+}
---
+++ FindMyMed/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FindMyMed.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public int VAT { get; set; }
+        public int PhoneNumber { get; set; }
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+    }
+}
