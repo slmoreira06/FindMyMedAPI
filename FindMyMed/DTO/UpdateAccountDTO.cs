64c2fb1a8c7fed2f0226b0adb390f55baea44c4b
Sara Moreira <a8544@alunos.ipca.pt>
2022-04-02 19:25:07 +0100

    Cleanup

--- FindMyMed/DTO/UpdateAccountDTO.cs
+++ FindMyMed/DTO/UpdateAccountDTO.cs
@@ -5,13 +5,8 @@ namespace FindMyMed.DTO
 {
     public class UpdateAccountDTO
     {
-        [Required]
         public string Email { get; set; }
-        [Required]
-        public string UserName { get; set; }
-        [Required]
         public string Password { get; set; }
-        [Required]
         public StatusEnum Status { get; set; }
     }
 }
