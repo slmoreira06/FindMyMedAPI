8483566dfa24743b1359907e66c13fdda91560a4
Sara Moreira <a8544@alunos.ipca.pt>
2022-04-06 15:16:55 +0100

    Fix

--- FindMyMed/DTO/LoginAccount.cs
+++ FindMyMed/DTO/LoginAccount.cs
@@ -1,9 +1,12 @@
-﻿namespace FindMyMed.DTO
+﻿using FindMyMed.Models;
+
+namespace FindMyMed.DTO
 {
     public class LoginAccount
     {
         public int? Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public StatusEnum Status { get; set; }
     }
 }
