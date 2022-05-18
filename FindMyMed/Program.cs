acd64e193df97bce7c3e381d4ff1c16dcfb3c493
Sara Moreira <a8544@alunos.ipca.pt>
2022-05-18 20:03:46 +0100

    Adding timer

--- FindMyMed/Program.cs
+++ FindMyMed/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IProductsRepository, ProductDAO>();
 builder.Services.AddScoped<ISupportsRepository, SupportDAO>();
 builder.Services.AddScoped<ICartsRepository, CartDAO>();
 builder.Services.AddScoped<IInventoriesRepository, InventoryDAO>();
+builder.Services.AddScoped<IRemindersRepository, ReminderDAO>();
 builder.Services.AddScoped<ICalendarEventsRepository, CalendarEventDAO>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen((options =>
